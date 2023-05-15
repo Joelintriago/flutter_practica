6f1bb508d72bf752ca73fb0db486e587057f0e1e
Joelintriago <david.intriagom@ug.edu.ec>
2023-05-15 15:23:20 -0500

    Hola

--- lib/main.dart
+++ lib/main.dart
@@ -35,78 +35,16 @@ class _InicioState extends State<Inicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body: cuerpo(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Home")
+          ],
+        ),
+      ),
     );
   }
 }
 
 
-Widget cuerpo(){
-  
-  return Container(
-    decoration: BoxDecoration(
-      image: DecorationImage(image: NetworkImage("https://i.pinimg.com/originals/3f/56/62/3f5662e8d76656be9fd769256f72e3da.jpg"),
-      fit: BoxFit.cover
-      )
-      
-    ),
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children:<Widget> [
-          nombre(),
-          campoUsuario(),
-          campoContrasena(),
-          SizedBox(height: 10,),
-          btnEntrar()
-        ],
-      ),
-      ),
-  );
-}
-
-Widget nombre(){
-  return Text("Joel Login", style: TextStyle(color: Colors.white,fontSize: 35.0, fontWeight: FontWeight.bold),);
-}
-
-Widget campoUsuario(){
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-    child: TextField(
-      decoration: InputDecoration(
-        hintText: "User",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-
-Widget campoContrasena(){
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
-    child: TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: "Password",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-
-Widget btnEntrar(){
-  return TextButton(
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
-      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 100,vertical: 10))
-      
-    ),
-    onPressed: (){}, 
-    child: Text("Entrar",style: TextStyle(fontSize: 25,color: Colors.white),)
-    );
-}
